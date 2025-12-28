4db15c86d9d20f0e73a1d69d36b0e35a7c3426c1
Riya Srivastava <riyasrivastava1701@gmail.com>
2025-12-28 23:54:24 +0530

    Restraunt Card Added

---
+++ dist/Swiggy.6bd02f5a.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "Hello from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=Swiggy.6bd02f5a.js.map
